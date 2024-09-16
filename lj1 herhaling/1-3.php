ebcc1c2c877659c8b758bfb4ec322eec34b900fd
Iain-Kanniainen <9022209@student.zadkine.nl>
2024-09-16 11:10:27 +0200

    1.3

---
+++ lj1 herhaling/1-3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $City = "Amsterdam";
+    $population = 821752;
+    $averageTemperature = 17.5;
+    $isCapital = true;
+
+    echo ("$City  $population  $averageTemperature  $isCapital");
+
+    ?>
+</body>
+</html>
