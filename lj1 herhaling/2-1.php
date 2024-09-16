c43b2eabe1799e457e6e9d321756d34d11ba6736
Iain-Kanniainen <9022209@student.zadkine.nl>
2024-09-16 12:51:21 +0200

    2.1

---
+++ lj1 herhaling/2-1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $age = 23;
+    if ($age >= 18 ) {
+        echo ("je bent volwassen");
+    }
+    else {
+        echo ("je bent niet volwassen");
+    }
+    
+    
+    ?>
+</body>
+</html>
