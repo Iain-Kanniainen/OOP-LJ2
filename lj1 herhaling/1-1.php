e79c98babf7f9e279eedb94d27c5e79b9aae8f33
Iain-Kanniainen <9022209@student.zadkine.nl>
2024-09-16 10:32:15 +0200

    Create 1-1.php

---
+++ lj1 herhaling/1-1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$firstName = "jan";
+$age = 25;;
+$height = 1.75;
+$isStudent = true;
+echo " $firstName 
+        $age
+        $height
+        $isStudent";
+?>
+
+</body>
+</html>
