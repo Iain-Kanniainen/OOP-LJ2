8da894a63d2744b8fae9f9969a47e0e15d58a3b3
Iain-Kanniainen <9022209@student.zadkine.nl>
2024-09-19 15:16:22 +0200

    3.3

---
+++ lj1 herhaling/3-3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+   $student = ["name"=>"Jan", "age" => 20, "grade" => "A"];
+   echo "Name of student: ".$student["name"]. "<br>";
+   echo "Age of student:" .$student["age"] . "<br>";
+   echo "Grade of student: ".$student["grade"]. "<br>";
+    ?>
+</body>
+</html>
